380abb365d6ba66b58866a5658a236258a6bdddf
Francisco <javiermartincuenca@gmail.com>
2019-04-10 20:34:31 +0200

    MVP interface added

--- app/src/main/java/com/example/moviedb/MainActivity.java
+++ app/src/main/java/com/example/moviedb/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.moviedb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.moviedb.movies.MoviesMVP;
+import com.example.moviedb.movies.ViewModel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements MoviesMVP.View {
+
+    private final String TAG = MainActivity.class.getName();
+
+    @BindView(R.id.activity_root_view)
+    ViewGroup rootView;
+
+    @BindView(R.id.recycler_view_movies)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void updateData(ViewModel viewModel) {
+
+    }
+
+    @Override
+    public void showSnackbar(String s) {
+
     }
 }
